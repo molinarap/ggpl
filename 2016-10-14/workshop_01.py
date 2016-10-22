13efd9b729a04d023025ab421c0f0d096812583e
Paolo Molinara <pmolinara@gmail.com>
2016-10-22 22:01:25 +0200

    colorazione dei pilastri e delle colonne

--- 2016-10-14/workshop_01.py
+++ 2016-10-14/workshop_01.py
@@ -1,9 +1,9 @@
 # workshop_01
 from larlib import *
 
-beams_weights = [3.0, 3.0, 3.0]
-pilars_heights = [5.0, 3.0, 3.0, 3.0, 2.0]
-plan_distance = [3.0, 5.0, 3.0, 3.0, 2.0]
+beams_weights = [4.0, 4.0]
+pilars_heights = [5.0, 2.5, 2.5]
+plan_distance = [3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0 ,3.0, 3.0, 3.0, 3.0]
 pilar_weight = 0.5 
 beam_weight = pilar_weight/2.0
 
@@ -13,9 +13,9 @@ arc = STRUCT([p0])
 
 # funzione che crea il telaio piano
 def createFloor(pilar_height_tot, beam_weight_tot, i, y, arc_all):
-	p1 = CUBOID([pilar_weight,pilar_weight,pilars_heights[y]])
+	p1 = COLOR(GREEN)(CUBOID([pilar_weight,pilar_weight,pilars_heights[y]]))
 	p1_t = STRUCT([T(2)(beam_weight_tot),T(3)(pilar_height_tot), p1])
-	t1 = CUBOID([beam_weight,beams_weights[i],beam_weight])
+	t1 = COLOR(RED)(CUBOID([beam_weight,beams_weights[i],beam_weight]))
 	t1_t = STRUCT([T([1,2,3])([beam_weight/2.0, (3.0*pilar_weight/4.0)+beam_weight_tot,pilar_height_tot+pilars_heights[y]]), t1])
 	beam_weight_tot=beam_weight_tot+beams_weights[i]+(pilar_weight/2.0)
 	if i == len(beams_weights) - 1:
