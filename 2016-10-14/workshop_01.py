6c80d6293d093a738bfaa1ec33eda05c1baa7995
Paolo Molinara <pmolinara@gmail.com>
2016-10-22 22:19:21 +0200

    un po di commenti

--- 2016-10-14/workshop_01.py
+++ 2016-10-14/workshop_01.py
@@ -1,18 +1,28 @@
-# workshop_01
+"""workshop_01"""
 from larlib import *
 
+"""
+beams_weights ---> lunghezza delle travi tra i pilastri dell'edificio
+pilars_heights ---> lunghezza dei pilastri dell'edificio
+pilar_weight ---> larghezza di un pilastro
+beam_weight ---> larghezza delle travi, sempre la metà dei pilastri in modo 
+				 che una trave possa sempre poggiare su un pilastro
+"""
 beams_weights = [4.0, 4.0]
 pilars_heights = [5.0, 2.5, 2.5]
-plan_distance = [3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0 ,3.0, 3.0, 3.0, 3.0]
 pilar_weight = 0.5 
 beam_weight = pilar_weight/2.0
 
-# inizializzo una struttura base da cui partire
+"""
+inizializzo una struttura vuota in cui verrà costruito il telaio
+"""
 p0 = CUBOID([0,0,0])
 arc = STRUCT([p0])
 
-# funzione che crea il telaio piano
-def createFloor(pilar_height_tot, beam_weight_tot, i, y, arc_all):
+"""
+funzione ricorsiva che crea un telaio partendo da un array di misure dei pilastri e delle travi
+"""
+def createFrame(pilar_height_tot, beam_weight_tot, i, y, arc_all):
 	p1 = COLOR(GREEN)(CUBOID([pilar_weight,pilar_weight,pilars_heights[y]]))
 	p1_t = STRUCT([T(2)(beam_weight_tot),T(3)(pilar_height_tot), p1])
 	t1 = COLOR(RED)(CUBOID([beam_weight,beams_weights[i],beam_weight]))
@@ -24,15 +34,15 @@ def createFloor(pilar_height_tot, beam_weight_tot, i, y, arc_all):
 		arc_all = STRUCT([arc_all,p1_t,t1_t,p1_next_t])
 		if y != len(pilars_heights) - 1:
 			y=y+1
-			return createFloor(pilar_height_tot, 0, 0, y, arc_all)
+			return createFrame(pilar_height_tot, 0, 0, y, arc_all)
 		else:
 			VIEW(arc_all)
 	else:
 		i=i+1
 		arc_all = STRUCT([arc_all,p1_t,t1_t])
-		return createFloor(pilar_height_tot, beam_weight_tot, i, y, arc_all)
+		return createFrame(pilar_height_tot, beam_weight_tot, i, y, arc_all)
 
-createFloor(0, 0, 0, 0, arc)
+createFrame(0, 0, 0, 0, arc)
 
 
 
--- 2016-10-21/workshop_02.py
+++ 2016-10-21/workshop_02.py
@@ -1,17 +1,28 @@
-# workshop_02
+"""workshop_02"""
 from larlib import *
 
+"""
+beams_weights ---> lunghezza delle travi tra i pilastri dell'edificio
+pilars_heights ---> lunghezza dei pilastri dell'edificio
+plan_distance ---> distanza tra un piano e l'altro all'interno dell'edificio
+pilar_weight ---> larghezza di un pilastro
+beam_weight ---> larghezza delle travi, sempre la metà dei pilastri in modo 
+				 che una trave possa sempre poggiare su un pilastro
+"""
 beams_weights = [4.0, 4.0]
 pilars_heights = [5.0, 2.5, 2.5]
 plan_distance = [3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0 ,3.0, 3.0, 3.0, 3.0]
 pilar_weight = 0.5 
 beam_weight = pilar_weight/2.0
 
-# inizializzo una struttura base da cui partire
-p0 = CUBOID([0,0,0])
+"""
+inizializzo una struttura vuota in cui verrà costruito il telaio
+"""p0 = CUBOID([0,0,0])
 arc = STRUCT([p0])
 
-# funzione che crea il telaio piano
+"""
+funzione ricorsiva che crea l'intera struttura andando a creare un piano sull'altro
+"""
 def createFloor(pilar_height_tot, beam_weight_tot, plan_distance_tot, x, z, y, arc_all):
 	if  z < len(pilars_heights): #1
 		p1 = COLOR(GREEN)(CUBOID([pilar_weight,pilar_weight,pilars_heights[z]]))
@@ -44,8 +55,10 @@ def createFloor(pilar_height_tot, beam_weight_tot, plan_distance_tot, x, z, y, a
 			dist = dist + p
 		return createLast(dist, 0, 0, 0, 0, arc_all)
 
-# funzione che crea il telaio piano
-def createLast(dist, pilar_height_tot, beam_weight_tot, i, y, arc_all):
+"""
+funzione ricorsiva che crea il telaio di chiusura della struttura
+"""
+def createLastFrame(dist, pilar_height_tot, beam_weight_tot, i, y, arc_all):
 	p1 = COLOR(GREEN)(CUBOID([pilar_weight,pilar_weight,pilars_heights[y]]))
 	p1_t = STRUCT([T(1)(dist),T(2)(beam_weight_tot),T(3)(pilar_height_tot), p1])
 	t1 = COLOR(RED)(CUBOID([beam_weight,beams_weights[i],beam_weight]))
@@ -57,13 +70,13 @@ def createLast(dist, pilar_height_tot, beam_weight_tot, i, y, arc_all):
 		arc_all = STRUCT([arc_all,p1_t,t1_t,p1_next_t])
 		if y != len(pilars_heights) - 1:
 			y=y+1
-			return createLast(dist, pilar_height_tot, 0, 0, y, arc_all)
+			return createLastFrame(dist, pilar_height_tot, 0, 0, y, arc_all)
 		else:
 			VIEW(arc_all)
 	else:
 		i=i+1
 		arc_all = STRUCT([arc_all,p1_t,t1_t])
-		return createLast(dist, pilar_height_tot, beam_weight_tot, i, y, arc_all)
+		return createLastFrame(dist, pilar_height_tot, beam_weight_tot, i, y, arc_all)
 
 createFloor(0, 0, 0, 0, 0, 0, arc)
 
