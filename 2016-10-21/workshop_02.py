0d97da2ba708855c74c94fde887f607321f45805
Paolo Molinara <pmolinara@gmail.com>
2016-10-24 01:43:44 +0200

    camelCase

--- 2016-10-21/workshop_02.py
+++ 2016-10-21/workshop_02.py
@@ -2,88 +2,89 @@
 from larlib import *
 import csv
 """
-beams_weights ---> lunghezza delle travi tra i pilastri dell'edificio
-pilars_heights ---> lunghezza dei pilastri dell'edificio
-plan_distance ---> distanza tra un piano e l'altro all'interno dell'edificio
-pilar_weight ---> larghezza di un pilastro
-beam_weight ---> larghezza delle travi, sempre la meta' dei pilastri in modo 
+beamsWeights ---> lunghezza delle travi tra i pilastri dell'edificio
+pillarsHeights ---> lunghezza dei pilastri dell'edificio
+planDistance ---> distanza tra un piano e l'altro all'interno dell'edificio
+pillarWeight ---> larghezza di un pilastro
+beamWeight ---> larghezza delle travi, sempre la meta' dei pilastri in modo 
 				 che una trave possa sempre poggiare su un pilastro
 """
-beams_weights = [4.0, 4.0]
-pilars_heights = [5.0, 2.5, 2.5]
-plan_distance = [3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0 ,3.0, 3.0, 3.0, 3.0]
-pilar_weight = 0.5 
-beam_weight = pilar_weight/2.0
+beamsWeights = [4.0, 4.0]
+pillarsHeights = [5.0, 2.5, 2.5]
+planDistance = [3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0 ,3.0, 3.0, 3.0, 3.0]
+pillarWeight = 0.5 
+beamWeight = pillarWeight/2.0
 
-with open('./data.csv', 'rb') as csvfile:
-    dialect = csv.Sniffer().sniff(csvfile.read(1024))
-    csvfile.seek(0)
-    reader = csv.reader(csvfile, dialect)
+# with open('./data.csv', 'rb') as csvfile:
+#     dialect = csv.Sniffer().sniff(csvfile.read(1024))
+#     csvfile.seek(0)
+#     reader = csv.reader(csvfile, dialect)
+    
 """
 inizializzo una struttura vuota in cui verra' costruito il telaio
 """
-p0 = CUBOID([0,0,0])
-arc = STRUCT([p0])
+firstPillar = CUBOID([0,0,0])
+initStruct = STRUCT([firstPillar])
 
 """
 funzione ricorsiva che crea l'intera struttura andando a creare un piano sull'altro
 """
-def createFloor(pilar_height_tot, beam_weight_tot, plan_distance_tot, x, z, y, arc_all):
-	if  z < len(pilars_heights): #1
-		p1 = COLOR(GREEN)(CUBOID([pilar_weight,pilar_weight,pilars_heights[z]]))
-		p1_t = STRUCT([T(1)(plan_distance_tot),T(2)(beam_weight_tot),T(3)(pilar_height_tot), p1])
-		t1 = COLOR(RED)(CUBOID([beam_weight,beams_weights[y],beam_weight]))
-		t1_t = STRUCT([T([1,2,3])([beam_weight/2.0+plan_distance_tot, (3.0*pilar_weight/4.0)+beam_weight_tot, pilar_height_tot+pilars_heights[z]]), t1])
-		if  x < len(plan_distance): #0
-			arc_all = STRUCT([arc_all,p1_t,t1_t])
-			t2 = COLOR(RED)(CUBOID([plan_distance[x],beam_weight,beam_weight]))
-			t2_t = STRUCT([T([1,2,3])([beam_weight/2.0+plan_distance_tot, (pilar_weight/4.0)+beam_weight_tot, pilar_height_tot+pilars_heights[z]]), t2])
-			beam_weight_tot=beam_weight_tot+beams_weights[y]+(pilar_weight/2.0)
-			if y < len(beams_weights) - 1: #0
+def createFloor(pillarHeightTot, beamWeightTot, planDistanceTot, x, z, y, struct):
+	if  z < len(pillarsHeights): #1
+		pillar = COLOR(GREEN)(CUBOID([pillarWeight,pillarWeight,pillarsHeights[z]]))
+		pillarT = STRUCT([T(1)(planDistanceTot),T(2)(beamWeightTot),T(3)(pillarHeightTot), pillar])
+		beam = COLOR(RED)(CUBOID([beamWeight,beamsWeights[y],beamWeight]))
+		beamT = STRUCT([T([1,2,3])([beamWeight/2.0+planDistanceTot, (3.0*pillarWeight/4.0)+beamWeightTot, pillarHeightTot+pillarsHeights[z]]), beam])
+		if  x < len(planDistance): #0
+			struct = STRUCT([struct,pillarT,beamT])
+			t2 = COLOR(RED)(CUBOID([planDistance[x],beamWeight,beamWeight]))
+			t2T = STRUCT([T([1,2,3])([beamWeight/2.0+planDistanceTot, (pillarWeight/4.0)+beamWeightTot, pillarHeightTot+pillarsHeights[z]]), t2])
+			beamWeightTot=beamWeightTot+beamsWeights[y]+(pillarWeight/2.0)
+			if y < len(beamsWeights) - 1: #0
 				y=y+1
-				arc_all = STRUCT([arc_all,t2_t])
-				return createFloor(pilar_height_tot, beam_weight_tot, plan_distance_tot, x, z, y, arc_all)
+				struct = STRUCT([struct,t2T])
+				return createFloor(pillarHeightTot, beamWeightTot, planDistanceTot, x, z, y, struct)
 			else:
-				p1_next_t = STRUCT([T(1)(plan_distance_tot),T(2)(beam_weight_tot),T(3)(pilar_height_tot), p1])
-				t2_next_t = STRUCT([T([1,2,3])([beam_weight/2.0+plan_distance_tot, (pilar_weight/4.0)+beam_weight_tot,pilar_height_tot+pilars_heights[z]]), t2])
-				arc_all = STRUCT([arc_all,t2_t,p1_next_t,t2_next_t])
-				plan_distance_tot = plan_distance_tot + plan_distance[x]
+				pillarNextT = STRUCT([T(1)(planDistanceTot),T(2)(beamWeightTot),T(3)(pillarHeightTot), pillar])
+				t2NextT = STRUCT([T([1,2,3])([beamWeight/2.0+planDistanceTot, (pillarWeight/4.0)+beamWeightTot,pillarHeightTot+pillarsHeights[z]]), t2])
+				struct = STRUCT([struct,t2T,pillarNextT,t2NextT])
+				planDistanceTot = planDistanceTot + planDistance[x]
 				x=x+1
-				return createFloor(pilar_height_tot, 0, plan_distance_tot, x, z, 0, arc_all)
+				return createFloor(pillarHeightTot, 0, planDistanceTot, x, z, 0, struct)
 		else:
-			pilar_height_tot = pilar_height_tot + pilars_heights[z] + beam_weight
+			pillarHeightTot = pillarHeightTot + pillarsHeights[z] + beamWeight
 			z=z+1
-			return createFloor(pilar_height_tot, 0, 0, 0, z, 0, arc_all)
+			return createFloor(pillarHeightTot, 0, 0, 0, z, 0, struct)
 	else:
 		dist = 0
-		for p in plan_distance:
+		for p in planDistance:
 			dist = dist + p
-		return createLastFrame(dist, 0, 0, 0, 0, arc_all)
+		return createLastFrame(dist, 0, 0, 0, 0, struct)
 
 """
 funzione ricorsiva che crea il telaio di chiusura della struttura
 """
-def createLastFrame(dist, pilar_height_tot, beam_weight_tot, i, y, arc_all):
-	p1 = COLOR(GREEN)(CUBOID([pilar_weight,pilar_weight,pilars_heights[y]]))
-	p1_t = STRUCT([T(1)(dist),T(2)(beam_weight_tot),T(3)(pilar_height_tot), p1])
-	t1 = COLOR(RED)(CUBOID([beam_weight,beams_weights[i],beam_weight]))
-	t1_t = STRUCT([T([1,2,3])([beam_weight/2.0+dist, (3.0*pilar_weight/4.0)+beam_weight_tot,pilar_height_tot+pilars_heights[y]]), t1])
-	beam_weight_tot=beam_weight_tot+beams_weights[i]+(pilar_weight/2.0)
-	if i == len(beams_weights) - 1:
-		p1_next_t = STRUCT([T(1)(dist),T(2)(beam_weight_tot),T(3)(pilar_height_tot), p1])
-		pilar_height_tot = pilar_height_tot + pilars_heights[y] + beam_weight
-		arc_all = STRUCT([arc_all,p1_t,t1_t,p1_next_t])
-		if y != len(pilars_heights) - 1:
+def createLastFrame(dist, pillarHeightTot, beamWeightTot, i, y, struct):
+	pillar = COLOR(GREEN)(CUBOID([pillarWeight,pillarWeight,pillarsHeights[y]]))
+	pillarT = STRUCT([T(1)(dist),T(2)(beamWeightTot),T(3)(pillarHeightTot), pillar])
+	beam = COLOR(RED)(CUBOID([beamWeight,beamsWeights[i],beamWeight]))
+	beamT = STRUCT([T([1,2,3])([beamWeight/2.0+dist, (3.0*pillarWeight/4.0)+beamWeightTot,pillarHeightTot+pillarsHeights[y]]), beam])
+	beamWeightTot=beamWeightTot+beamsWeights[i]+(pillarWeight/2.0)
+	if i == len(beamsWeights) - 1:
+		pillarNextT = STRUCT([T(1)(dist),T(2)(beamWeightTot),T(3)(pillarHeightTot), pillar])
+		pillarHeightTot = pillarHeightTot + pillarsHeights[y] + beamWeight
+		struct = STRUCT([struct,pillarT,beamT,pillarNextT])
+		if y != len(pillarsHeights) - 1:
 			y=y+1
-			return createLastFrame(dist, pilar_height_tot, 0, 0, y, arc_all)
+			return createLastFrame(dist, pillarHeightTot, 0, 0, y, struct)
 		else:
-			VIEW(arc_all)
+			VIEW(struct)
 	else:
 		i=i+1
-		arc_all = STRUCT([arc_all,p1_t,t1_t])
-		return createLastFrame(dist, pilar_height_tot, beam_weight_tot, i, y, arc_all)
+		struct = STRUCT([struct,pillarT,beamT])
+		return createLastFrame(dist, pillarHeightTot, beamWeightTot, i, y, struct)
 
-createFloor(0, 0, 0, 0, 0, 0, arc)
+createFloor(0, 0, 0, 0, 0, 0, initStruct)
 
 
 
