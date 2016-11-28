5e987fab1ec378f477ca93f1279967f18094da0d
Paolo Molinara <redpanda@MacBook-Pro-di-Paolo.local>
2016-11-28 11:54:12 +0100

    commenti

--- 2016-11-25/workshop_07.py
+++ 2016-11-25/workshop_07.py
@@ -27,20 +27,21 @@ xDoor2 = [0.0,0.2,0.4,0.6,0.8,1.0]
 yDoor2 = [0.0,0.2,0.9,1.0,1.6,1.8]
 occDoor2 = [[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1],[1,0,1,0,1],[1,1,1,1,1]]
 
-
+# struttura iniziale
 zero = QUOTE([.0,.0])
 initStruct = STRUCT([zero])
 
+# funzione che costruisce una finestra
 def windows(i, j, s):
 	if j != len(yWindow1)-1:
 		if  i != len(xWindow1)-1:
 			if occWindow1[j][i]==0:
-				x = TEXTURE([glassTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([xWindow1[i+1]-xWindow1[i],yWindow1[j+1]-yWindow1[j],0.01]))
-				x_tras = STRUCT([T([1,2,3])([xWindow1[i],yWindow1[j],0.1/3]), x])
+				piece = TEXTURE([glassTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([xWindow1[i+1]-xWindow1[i],yWindow1[j+1]-yWindow1[j],0.01]))
+				pieceTras = STRUCT([T([1,2,3])([xWindow1[i],yWindow1[j],0.1/3]), piece])
 			else:
-				x = TEXTURE([darkWoodTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([xWindow1[i+1]-xWindow1[i],yWindow1[j+1]-yWindow1[j],0.1]))
-				x_tras = STRUCT([T([1,2])([xWindow1[i],yWindow1[j]]), x])
-			s = STRUCT([s,x_tras])
+				piece = TEXTURE([darkWoodTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([xWindow1[i+1]-xWindow1[i],yWindow1[j+1]-yWindow1[j],0.1]))
+				pieceTras = STRUCT([T([1,2])([xWindow1[i],yWindow1[j]]), piece])
+			s = STRUCT([s,pieceTras])
 			return windows(i+1, j, s)
 		else:
 			return windows(0, j+1, s)
@@ -49,27 +50,28 @@ def windows(i, j, s):
 
 windows(0, 0, initStruct)
 
+# funzione che costruisce una porta
 def doors(i, j, s):
 	if j != len(yDoor1)-1:
 		if  i != len(xDoor1)-1:
 			if occDoor1[j][i]==0:
-				x = TEXTURE([metalTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([xDoor1[i+1]-xDoor1[i],yDoor1[j+1]-yDoor1[j],0.1]))
-				x_tras = STRUCT([T([1,2,3])([xDoor1[i],yDoor1[j],0.0]), x])
+				piece = TEXTURE([metalTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([xDoor1[i+1]-xDoor1[i],yDoor1[j+1]-yDoor1[j],0.1]))
+				pieceTras = STRUCT([T([1,2,3])([xDoor1[i],yDoor1[j],0.0]), piece])
 			else:
-				x = TEXTURE([whiteWoodTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([xDoor1[i+1]-xDoor1[i],yDoor1[j+1]-yDoor1[j],0.1]))
-				x_tras = STRUCT([T([1,2])([xDoor1[i],yDoor1[j]]), x])
-			s = STRUCT([s,x_tras])
+				piece = TEXTURE([whiteWoodTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([xDoor1[i+1]-xDoor1[i],yDoor1[j+1]-yDoor1[j],0.1]))
+				pieceTras = STRUCT([T([1,2])([xDoor1[i],yDoor1[j]]), piece])
+			s = STRUCT([s,pieceTras])
 			return doors(i+1, j, s)
 		else:
 			return doors(0, j+1, s)
 	else:
-		man = TEXTURE([metalTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([0.2,0.05,0.05]))
-		man = STRUCT([T([1,2,3])([0.0,0.0,0.15]), man])
-		man_1 = TEXTURE([metalTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([0.05,0.05,0.05]))
-		man_1 = STRUCT([T([1,2,3])([0.0,0.0,0.1]), man_1])
-		man_tras = STRUCT([man,man_1])
-		man_tras = STRUCT([T([1,2,3])([xDoor1[i]+0.1,yDoor1[j]/2,0.0]), man_tras])
-		s = STRUCT([s,man_tras])
+		handle = TEXTURE([metalTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([0.2,0.05,0.05]))
+		handle = STRUCT([T([1,2,3])([0.0,0.0,0.15]), handle])
+		handle_1 = TEXTURE([metalTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([0.05,0.05,0.05]))
+		handle_1 = STRUCT([T([1,2,3])([0.0,0.0,0.1]), handle_1])
+		handle_tras = STRUCT([handle,handle_1])
+		handle_tras = STRUCT([T([1,2,3])([xDoor1[i]+0.1,yDoor1[j]/2,0.0]), handle_tras])
+		s = STRUCT([s,handle_tras])
 		VIEW(s)
 
 doors(0, 0, initStruct)
