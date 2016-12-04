16371f82be0e507cd4ecfba3badeef91fb4677a1
Paolo Molinara <redpanda@mbp-di-paolo.homenet.telecomitalia.it>
2016-12-04 16:54:59 +0100

    commenti

--- 2016-12-02/workshop_08.py
+++ 2016-12-02/workshop_08.py
@@ -17,13 +17,13 @@ def readFile(l):
 	file.close()
 	return data.splitlines()
 
-# contorno della piantina
+""" contorno della piantina """
 level1 = readFile(1)
-# piantina con porte
+""" piantina con porte """
 level2 = readFile(2)
-# piantina con le finestre e le porte
+""" piantina con le finestre e le porte """
 level3 = readFile(3)
-# piantina con stipidi delle porte
+""" piantina con stipidi delle porte """
 level4 = readFile(4)
 
 """ inserisco tutto in un array che andrò a scorrere per costruire un piano sull'altro"""
