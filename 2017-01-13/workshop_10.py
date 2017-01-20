669d6f62c89cb31f1c99e901286986de593cdb04
molinarap <pmolinara@gmail.com>
2017-01-20 16:17:59 +0100

    fix contatore

--- 2017-01-13/workshop_10.py
+++ 2017-01-13/workshop_10.py
@@ -27,6 +27,7 @@ def readSvg(l,reading_level,path):
 
 levelBase = readSvg(0,[],"base")
 levelEst = readSvg(0,[],"esterno")
+levelInt = readSvg(0,[],"interno")
 
 def createBase(i,s1):
   if i < len(levelBase[0]):
@@ -44,7 +45,7 @@ def createBase(i,s1):
 
 def createEst(l,i,h,s1):
   if l <= len(levelEst)-1:
-    if i < len(levelEst[l])-1:
+    if i < len(levelEst[l]):
       a = levelEst[l][i]
       a_split = a.split(",")
       a_number = np.array(a_split, dtype=float)
@@ -66,29 +67,30 @@ def createEst(l,i,h,s1):
   else:
     return s1
 
-# def createInt(l,i,h,s1,level):
-#   if l <= len(levelInt)-1:
-#     if i < len(levelInt[l])-1:
-#       a = levelInt[l][i]
-#       a_split = a.split(",")
-#       a_number = np.array(a_split, dtype=float)
-#       a_pol = MKPOL([[[a_number[0],a_number[1],0.0],[a_number[0],a_number[3],0.0],[a_number[2],a_number[1],0.0],[a_number[2],a_number[3],0.0]],[[1,2,3,4]],[1]])
-#       a_off = OFFSET([1.0, 1.0, level_height[l]])(a_pol)
-#       a_texture = TEXTURE([wallTexture, TRUE, FALSE, 1, 1, 0, 2, 1])(a_off)
-#       a_tras = STRUCT([T(3)(h), a_texture])
-#       s2 = STRUCT([a_tras, s1])
-#       return createInt(l,i+1,h,s2,level)
-#     else:
-#       h = h + level_height[l]
-#       return createInt(l+1,0,h,s1,level)
-#   else:
-#     return s1
+def createInt(l,i,h,s1):
+  if l <= len(levelInt)-1:
+    if i < len(levelInt[l]):
+      a = levelInt[l][i]
+      a_split = a.split(",")
+      a_number = np.array(a_split, dtype=float)
+      a_pol = MKPOL([[[a_number[0],a_number[1],0.0],[a_number[0],a_number[3],0.0],[a_number[2],a_number[1],0.0],[a_number[2],a_number[3],0.0]],[[1,2,3,4]],[1]])
+      a_off = OFFSET([1.0, 1.0, level_height[l]])(a_pol)
+      a_texture = TEXTURE([wallTexture, TRUE, FALSE, 1, 1, 0, 2, 1])(a_off)
+      a_tras = STRUCT([T(3)(h), a_texture])
+      s2 = STRUCT([a_tras, s1])
+      return createInt(l,i+1,h,s2)
+    else:
+      h = h + level_height[l]
+      return createInt(l+1,0,h,s1)
+  else:
+    return s1
 
 def createHouse():
   a = createBase(0,initStruct)
   b = createEst(0,0,0.0,initStruct)
-  # c = createInt(1,0,0.0,initStruct,levelInt)
+  c = createInt(0,0,0.0,initStruct)
   house=STRUCT([a,T(3)(3.0),b])
+  house=STRUCT([house,T(3)(3.5),c])
   house=STRUCT([house,T(3)(83.0),a])
   house=STRUCT([house,T(3)(163.0),a])
   #house = SKEL_3(house)
