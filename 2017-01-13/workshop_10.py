9e844ca1573c19c7333d814fad7e329c9d9c8ab0
molinarap <pmolinara@gmail.com>
2017-01-24 18:34:48 +0100

    funzione che crea porta singola, maniglia e che poi richiamata va a creare tutte le porte delle struttura

--- 2017-01-13/workshop_10.py
+++ 2017-01-13/workshop_10.py
@@ -31,12 +31,12 @@ def readSvg(l,reading_level,path):
     return reading_level
 
 levelBase = readSvg(0,[],"base")
-levelEst = readSvg(0,[],"esterno")
-levelInt = readSvg(0,[],"interno")
-levelDoors = readSvg(0,[],"porte")
-levelWindows = readSvg(0,[],"finestre")
+levelExternal = readSvg(0,[],"external")
+levelInternal = readSvg(0,[],"internal")
+levelDoors = readSvg(0,[],"doors")
+levelWindows = readSvg(0,[],"windows")
 
-def createBase(i,s1):
+def buildBase(i,s1):
   if i < len(levelBase[0]):
     a = levelBase[0][i]
     a_split = a.split(",")
@@ -44,16 +44,16 @@ def createBase(i,s1):
     a_pol = POLYLINE([[a_number[0],a_number[1]],[a_number[2],a_number[3]]])
     a_off = OFFSET([4.0, 5.5, 2.0])(a_pol)
     s2 = STRUCT([a_off, s1])
-    return createBase(i+1,s2)
+    return buildBase(i+1,s2)
   else:
     s1 = SOLIDIFY(SKEL_2(s1))
     s1 = TEXTURE([pavTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(s1)
     return s1
 
-def createEst(l,i,h,s1):
-  if l <= len(levelEst)-1:
-    if i < len(levelEst[l]):
-      a = levelEst[l][i]
+def buildExternal(l,i,h,s1):
+  if l <= len(levelExternal)-1:
+    if i < len(levelExternal[l]):
+      a = levelExternal[l][i]
       a_split = a.split(",")
       a_number = np.array(a_split, dtype=float)
       a_pol = MKPOL([[[a_number[0],a_number[1],0.0],[a_number[0],a_number[3],0.0],[a_number[2],a_number[1],0.0],[a_number[2],a_number[3],0.0]],[[1,2,3,4]],[1]])
@@ -67,17 +67,17 @@ def createEst(l,i,h,s1):
           a_texture = TEXTURE([wallTexture, TRUE, FALSE, 1, 1, 0, 6, 1])(a_off)
       a_tras = STRUCT([T(3)(h), a_texture])
       s2 = STRUCT([a_tras, s1])
-      return createEst(l,i+1,h,s2)
+      return buildExternal(l,i+1,h,s2)
     else:
       h = h + level_height[l]
-      return createEst(l+1,0,h,s1)
+      return buildExternal(l+1,0,h,s1)
   else:
     return s1
 
-def createInt(l,i,h,s1):
-  if l <= len(levelInt)-1:
-    if i < len(levelInt[l]):
-      a = levelInt[l][i]
+def buildIntenal(l,i,h,s1):
+  if l <= len(levelInternal)-1:
+    if i < len(levelInternal[l]):
+      a = levelInternal[l][i]
       a_split = a.split(",")
       a_number = np.array(a_split, dtype=float)
       a_pol = MKPOL([[[a_number[0],a_number[1],0.0],[a_number[0],a_number[3],0.0],[a_number[2],a_number[1],0.0],[a_number[2],a_number[3],0.0]],[[1,2,3,4]],[1]])
@@ -85,38 +85,78 @@ def createInt(l,i,h,s1):
       a_texture = TEXTURE([wallTexture, TRUE, FALSE, 1, 1, 0, 2, 1])(a_off)
       a_tras = STRUCT([T(3)(h), a_texture])
       s2 = STRUCT([a_tras, s1])
-      return createInt(l,i+1,h,s2)
+      return buildIntenal(l,i+1,h,s2)
     else:
       h = h + level_height[l]
-      return createInt(l+1,0,h,s1)
+      return buildIntenal(l+1,0,h,s1)
   else:
     return s1
 
-def buildDoors(l,i,h,s1):
+def createHandle(elem,s1):
+  handle = MKPOL([[[elem[0],elem[1],0.0],[elem[2],elem[3],0.0]],[[1,2]],[1]])
+  handle0 = OFFSET([8.0, 1.0, 1.0])(handle)
+  if (elem[0]-elem[2]<1.0) & (elem[0]-elem[2]>-1.0):
+    handle_0 = STRUCT([T([1,2,3])([-2.0,1.0,31.0]), handle0])
+    handle_1 = STRUCT([T([1,2,3])([-2.0,3.0,31.0]), handle0])
+    handle1 = OFFSET([1.0, 1.5, 1.0])(handle)
+    handle_2 = STRUCT([T([1,2,3])([5.0,1.0,31.0]), handle1])
+    handle_3 = STRUCT([T([1,2,3])([5.0,6.0,31.0]), handle1])
+    handle_01 = DIFF([handle_0,handle_1])
+    handle_23 = DIFF([handle_2,handle_3])
+    handle_23_2 = STRUCT([T([1])([-8.0]), handle_23])
+  else:
+    handle = MKPOL([[[elem[0],elem[1],0.0],[elem[2],elem[3],0.0]],[[1,2]],[1]])
+    handle0 = OFFSET([1.0, 8.0, 1.0])(handle)
+    handle_0 = STRUCT([T([1,2,3])([1.0,-2.0,31.0]), handle0])
+    handle_1 = STRUCT([T([1,2,3])([3.0,-2.0,31.0]), handle0])
+    handle1 = OFFSET([1.0, 1.5, 1.0])(handle)
+    handle_2 = STRUCT([T([1,2,3])([1.0,5.0,31.0]), handle1])
+    handle_3 = STRUCT([T([1,2,3])([6.0,5.0,31.0]), handle1])
+    handle_01 = DIFF([handle_0,handle_1])
+    handle_23 = DIFF([handle_2,handle_3])
+    handle_23_2 = STRUCT([T([2])([-8.0]), handle_23])
+  handle_all = STRUCT([handle_01,handle_23,handle_23_2])
+  handle_all = TEXTURE([metalTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(handle_all)
+  s1 = STRUCT([s1,handle_all])
+  return s1
+
+# funzione che costruisce una porta
+def buildOneDoor(elem, j, h, door):
+  if j < 13:
+    build = MKPOL([[[elem[0],elem[1],0.0],[elem[2],elem[3],0.0]],[[1,2]],[1]])
+    if not j%2:
+      buildOffset = OFFSET([3.5, 3.5, 8.5])(build)
+      buildTexture = TEXTURE([pavTexture, TRUE, FALSE, 1, 1, 0, 6, 1])(buildOffset)
+      buildTras = STRUCT([T([3])([h]), buildTexture])
+      h = h + 8.5
+    else:
+      buildOffset = OFFSET([3.5, 3.5, 0.5])(build)
+      buildTexture = TEXTURE([metalTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(buildOffset)
+      buildTras = STRUCT([T([3])([h]), buildTexture])
+      h = h + 0.5
+    door = STRUCT([buildTras,door])
+    return buildOneDoor(elem, j+1, h, door)
+  else:
+    return door
+
+def buildAllDoors(l,i,h,s1):
   if l <= len(levelDoors)-1:
     if i < len(levelDoors[l]):
       coords = levelDoors[l][i]
       arrayCoords = coords.split(",")
       elem = np.array(arrayCoords, dtype=float)
-      build = MKPOL([[[elem[0],elem[1],0.0],[elem[0],elem[3],0.0],[elem[2],elem[1],0.0],[elem[2],elem[3],0.0]],[[1,2,3,4]],[1]])
-      buildOffset = OFFSET([3.1, 3.1, 60.0])(build)
-      buildTexture = TEXTURE([doorTexture, TRUE, FALSE, 1, 1, 0, 6, 1])(buildOffset)
-      buildTraslated = STRUCT([T(3)(h), buildTexture])
-      finalStruct = STRUCT([buildTraslated, s1])
-      #maniglia cuboidale
-      handle_1 = TEXTURE([metalTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(CUBOID([5,5,5]))
-      handle_1 = STRUCT([T([1,2,3])([1.5,1.5,h+30.0]), handle_1])
-      handle_tras = STRUCT([handle_1])
-      handle_tras = STRUCT([T([1,2,3])([elem[0],elem[1],0.0]), handle_tras])
-      finalStruct = STRUCT([finalStruct,handle_tras])
-      return buildDoors(l,i+1,h,finalStruct)
+      d = buildOneDoor(elem, 0, h, initStruct)
+      hand = createHandle(elem,initStruct)
+      hand = STRUCT([T([3])([h]), hand])
+      finalStruct = STRUCT([d, s1, hand])
+      return buildAllDoors(l,i+1,h,finalStruct)
     else:
       h = h + 80.0 #se e' una porta tiro su di 60
-      return buildDoors(l+1,0,h,s1)
+      return buildAllDoors(l+1,0,h,s1)
   else:
     return s1
 
-def build(elem,h):
+def buildOneWindow(elem,h):
   q1 = MKPOL([[[elem[0],elem[1],0.0],[elem[2],elem[3],0.0]],[[1,2]],[1]])
   q1 = OFFSET([3.5, 3.5, 30.0])(q1)
   q1 = TEXTURE([glassTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(q1)
@@ -136,22 +176,30 @@ def build(elem,h):
   q = TEXTURE([pavTexture, TRUE, FALSE, 1, 1, 0, 1, 1])(q2)
   allWindow = STRUCT([q, q1])
   allWindow = STRUCT([T(3)(h), allWindow])
+  # if (elem[0]-elem[2]<1.0) & (elem[0]-elem[2]>-1.0):
+  #   if elem[1]<elem[3]:
+  #     allWindow = STRUCT([T(1)(1.0),allWindow])
+  #   else:
+  #     allWindow = STRUCT([T(1)(0.0),allWindow])
+  # if (elem[1]-elem[3]<1.0) & (elem[1]-elem[3]>-1.0):
+  #   if elem[0]<elem[2]:
+  #     allWindow = STRUCT([T(2)(1.0),allWindow])
+  #   else:
+  #     allWindow = STRUCT([T(2)(-1.0),allWindow])
   return allWindow
 
-#build([251.89999389648438,-221.86500549316406,212.3260040283203,-221.86500549316406], 20)
-
-def buildWindows(l,i,h,s1):
+def buildAllWindows(l,i,h,s1):
   if l <= len(levelWindows)-1:
     if i < len(levelWindows[l]):
       coords = levelWindows[l][i]
       arrayCoords = coords.split(",")
       elem = np.array(arrayCoords, dtype=float)
-      w = build(elem,h)
+      w = buildOneWindow(elem,h)
       finalStruct = STRUCT([w, s1])
-      return buildWindows(l,i+1,h,finalStruct)
+      return buildAllWindows(l,i+1,h,finalStruct)
     else:
-      h = h + 80.0 #se e' una porta tiro su di 60
-      return buildWindows(l+1,0,h,s1)
+      h = h + 80.0
+      return buildAllWindows(l+1,0,h,s1)
   else:
     return s1
 
@@ -179,12 +227,12 @@ def createRoof(n,s1):
   s2 = STRUCT([roof, s2])
   return s2
 
-def createHouse():
-  a = createBase(0,initStruct)
-  b = createEst(0,0,0.0,initStruct)
-  c = createInt(0,0,0.0,initStruct)
-  d = buildDoors(0,0,0.0,initStruct)
-  e = buildWindows(0,0,30.0,initStruct)
+def buildHouse():
+  a = buildBase(0,initStruct)
+  b = buildExternal(0,0,0.0,initStruct)
+  c = buildIntenal(0,0,0.0,initStruct)
+  d = buildAllDoors(0,0,0.0,initStruct)
+  e = buildAllWindows(0,0,30.0,initStruct)
   f = createRoof(0,initStruct)
   g = createRoof(3,initStruct)
   house=STRUCT([a,T(3)(3.0),b])
@@ -193,8 +241,8 @@ def createHouse():
   house=STRUCT([house,T(3)(163.0),a])
   house=STRUCT([house,T(3)(3.0),d])
   house=STRUCT([house,T(3)(3.0),e])
-  house=STRUCT([house,T(3)(165.0),f])
-  house=STRUCT([house,T(3)(165.0),g])
+  house=STRUCT([house,T(3)(163.0),f])
+  house=STRUCT([house,T(3)(163.0),g])
   VIEW(house)
 
-createHouse()
+buildHouse()
