c2c11f03b3da98a5e71cf81e6778bef649429941
molinarap <pmolinara@gmail.com>
2017-01-24 12:30:48 +0100

    funzione createRoof parametrica

--- 2017-01-13/workshop_10.py
+++ 2017-01-13/workshop_10.py
@@ -147,44 +147,28 @@ def buildWindows(l,i,h,s1):
   else:
     return s1
 
-
-def createRoof1(s1):
-  a = levelBase[0][0]
-  a_split = a.split(",")
-  a_number = np.array(a_split, dtype=float)
-  pezzo = MKPOL([[[a_number[0],a_number[1],0.0],[a_number[2],a_number[3],0.0],[(a_number[2]+a_number[0])/2,(a_number[3]+a_number[1])/2,30.0]],[[1,2,3]],[1]])
-  linea = MKPOL([[[a_number[0],a_number[1]],[(a_number[2]+a_number[0])/2-30.0,(a_number[3]+a_number[1])/2]],[[1,2]],[1]])
-  a_pezzo = OFFSET([-3.0, 0.0, 0.0])(pezzo)
-  a_pezzo = TEXTURE([wallTexture, TRUE, FALSE, 1, 1, 0, 2, 1])(a_pezzo)
-  lung = levelBase[0][2]
-  lung_split = lung.split(",")
-  lung_number = np.array(lung_split, dtype=float)
-  lunghezza = lung_number[2] - lung_number[0]
-  a_prod = STRUCT([T([1,3])([lung_number[0]-8,a_number[0]]),(ROTATE([1,3])(-PI/2)(PROD([linea,Q(lunghezza+10)])))])
-  a_prod = OFFSET([3.0, 3.0, 3.0])(a_prod)
-  a_prod = TEXTURE([roofTexture, TRUE, FALSE, 1, 1, 0, 2, 1])(a_prod)
-  s2 = STRUCT([a_pezzo, s1])
-  s2 = STRUCT([a_prod, s2])
-  return s2
-
-def createRoof2(s1):
-  a = levelBase[0][3]
-  a_split = a.split(",")
-  a_number = np.array(a_split, dtype=float)
-  pezzo = MKPOL([[[a_number[0],a_number[1],0.0],[a_number[2],a_number[3],0.0],[(a_number[2]+a_number[0])/2,(a_number[3]+a_number[1])/2,30.0]],[[1,2,3]],[1]])
-  linea = MKPOL([[[a_number[2],a_number[3]],[(a_number[2]+a_number[0])/2-30.0,(a_number[3]+a_number[1])/2]],[[1,2]],[1]])
-  a_pezzo = OFFSET([3.0, 0.0, 0.0])(pezzo)
-  a_pezzo = TEXTURE([wallTexture, TRUE, FALSE, 1, 1, 0, 2, 1])(a_pezzo)
-  lung = levelBase[0][2]
-  lung_split = lung.split(",")
-  lung_number = np.array(lung_split, dtype=float)
-  lunghezza = lung_number[2] - lung_number[0]
-  print(lunghezza)
-  a_prod = STRUCT([T([1,3])([lung_number[0]-8,a_number[0]]),(ROTATE([1,3])(-PI/2)(PROD([linea,Q(lunghezza+10)])))])
-  a_prod = OFFSET([3.0, 3.0, 3.0])(a_prod)
-  a_prod = TEXTURE([roofTexture, TRUE, FALSE, 1, 1, 0, 2, 1])(a_prod)
-  s2 = STRUCT([a_pezzo, s1])
-  s2 = STRUCT([a_prod, s2])
+def createRoof(n,s1):
+  base = levelBase[0][n]
+  base_split = base.split(",")
+  base_number = np.array(base_split, dtype=float)
+  if n==0:
+    truss = MKPOL([[[base_number[0],base_number[1],0.0],[base_number[2],base_number[3],0.0],[(base_number[2]+base_number[0])/2,(base_number[3]+base_number[1])/2,30.0]],[[1,2,3]],[1]])
+    roof_frame = MKPOL([[[base_number[0],base_number[1]],[(base_number[2]+base_number[0])/2-30.0,(base_number[3]+base_number[1])/2]],[[1,2]],[1]])
+    truss = OFFSET([-3.0, 0.0, 0.0])(truss)
+  else:
+      truss = MKPOL([[[base_number[0],base_number[1],0.0],[base_number[2],base_number[3],0.0],[(base_number[2]+base_number[0])/2,(base_number[3]+base_number[1])/2,30.0]],[[1,2,3]],[1]])
+      roof_frame = MKPOL([[[base_number[2],base_number[3]],[(base_number[2]+base_number[0])/2-30.0,(base_number[3]+base_number[1])/2]],[[1,2]],[1]])
+      truss = OFFSET([3.0, 0.0, 0.0])(truss)
+  truss = TEXTURE([wallTexture, TRUE, FALSE, 1, 1, 0, 2, 1])(truss)
+  roof_height = levelBase[0][2]
+  roof_height_split = roof_height.split(",")
+  roof_height_number = np.array(roof_height_split, dtype=float)
+  roof_height = roof_height_number[2] - roof_height_number[0]
+  roof = STRUCT([T([1,3])([roof_height_number[0]-8,base_number[0]-1]),(ROTATE([1,3])(-PI/2)(PROD([roof_frame,Q(roof_height+10)])))])
+  roof = OFFSET([3.0, 3.0, 3.0])(roof)
+  roof = TEXTURE([roofTexture, TRUE, FALSE, 1, 1, 0, 2, 1])(roof)
+  s2 = STRUCT([truss, s1])
+  s2 = STRUCT([roof, s2])
   return s2
 
 def createHouse():
@@ -193,17 +177,16 @@ def createHouse():
   c = createInt(0,0,0.0,initStruct)
   d = buildDoors(0,0,0.0,initStruct)
   e = buildWindows(0,0,30.0,initStruct)
-  f = createRoof1(initStruct)
-  g = createRoof2(initStruct)
+  f = createRoof(0,initStruct)
+  g = createRoof(3,initStruct)
   house=STRUCT([a,T(3)(3.0),b])
   house=STRUCT([house,T(3)(3.5),c])
   house=STRUCT([house,T(3)(83.0),a])
   house=STRUCT([house,T(3)(163.0),a])
   house=STRUCT([house,T(3)(3.0),d])
   house=STRUCT([house,T(3)(3.0),e])
-  house=STRUCT([house,T(3)(166.0),f])
-  house=STRUCT([house,T(3)(166.0),g])
-  #house = SKEL_3(house)
+  house=STRUCT([house,T(3)(165.0),f])
+  house=STRUCT([house,T(3)(165.0),g])
   VIEW(house)
 
 createHouse()
